a78f9b13b400c7f98dd1ae8dabb5891aa42aad02
Inka <bohunicka.ingrid@gmail.com>
2019-02-07 15:46:26 +0100

    timer added

--- ActivityLoggerApp.py
+++ ActivityLoggerApp.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from mongoengine import *
+from Activity import *
 
 class ActivityLoggerApp(Tk):
     def __init__(self):
@@ -8,23 +9,28 @@ class ActivityLoggerApp(Tk):
         main = Frame(self, width=1000)
         main.pack()
 
+        self.duration = 0 # in seconds
+        self.activity_is_stoppped = True
+
         ## activity frame
         activity_label_frame = LabelFrame(main, text='Activity')
         activity_label_frame.pack(side=TOP, fill=BOTH)
         label = Label(activity_label_frame, text='Activity:')
         label.pack(side=LEFT)
-        activity_name = Entry(activity_label_frame, width=30)
-        activity_name.pack(side=LEFT)
+
+        self.content = StringVar()
+        activity_name_entry = Entry(activity_label_frame, width=30, textvariable=self.content)
+        activity_name_entry.pack(side=LEFT)
         add_button = Button(activity_label_frame, text='Add', width=13, command = self.add_activity)
         add_button.pack(side=LEFT)
 
         ## timer frame
         timer_label_frame = LabelFrame(main, text='Timer')
         timer_label_frame.pack(fill=BOTH)
-        time_label = Label(timer_label_frame, text='0 s', width=30)
-        time_label.pack(side=LEFT)
-        start_stop_button = Button(timer_label_frame, text='START', width=13, command=self.start_activity)
-        start_stop_button.pack()
+        self.time_label = Label(timer_label_frame, text='{} s'.format(self.duration), width=30)
+        self.time_label.pack(side=LEFT)
+        self.start_stop_button = Button(timer_label_frame, text='START', width=13, command=self.start_activity)
+        self.start_stop_button.pack()
 
         ## statistics frame
         statistics_label_frame = LabelFrame(main, text='Statistics')
@@ -32,14 +38,28 @@ class ActivityLoggerApp(Tk):
         statistics_button = Button(statistics_label_frame, text='Generate statistics', command=self.generate_stats)
         statistics_button.pack()
 
+        self.timer()
+
     def generate_stats(self):
         pass
 
     def start_activity(self):
-        pass
+        if self.activity_is_stoppped:
+            self.activity_is_stoppped = False
+            self.start_stop_button.config(text='STOP')
+        else:
+            self.start_stop_button.config(text='START')
+            self.activity_is_stoppped = True
+
 
     def add_activity(self):
-        pass
+        self.activity_name = self.content.get()
+
+    def timer(self):
+        if not self.activity_is_stoppped:
+            self.time_label.config(text = '{} s'.format(self.duration))
+            self.duration += 1
+        self.after(1000, self.timer)
 
 
 if __name__ == '__main__':
